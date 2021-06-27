21e1691e3ce2dca31995ba092c31bdc168e0ba37
stephenavocado <stephenwalter@chicagobooth.edu>
2021-06-27 15:32:44 +0000

    back to basics

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -17,4 +17,5 @@
 #
 class Goal < ApplicationRecord
   belongs_to(:user, { :required => false, :class_name => "User", :foreign_key => "user_id", :counter_cache => true })
+  validates :target_date, after_date: Date.today
 end
