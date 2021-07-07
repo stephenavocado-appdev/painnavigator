92bbf0523cf8effa8f2e15f8a13963826fe9ffae
stephenavocado <stephenwalter@chicagobooth.edu>
2021-07-07 00:38:06 +0000

    added email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,10 @@
 class User < ApplicationRecord
   validates :first_name, presence: true, length: {maximum: 25}
   validates :last_name, presence: true, length: {maximum: 25}
-  validates :email, :uniqueness => { :case_sensitive => false }
-  validates :email, :presence => true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
 
   has_many(:goals, { :class_name => "Goal", :foreign_key => "user_id", :dependent => :destroy })
