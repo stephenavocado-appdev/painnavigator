af61a49c57e78a3857281a99da894fd5117b329f
stephenavocado <stephenwalter@chicagobooth.edu>
2021-07-07 00:41:20 +0000

    added before_save email downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@
 #  updated_at         :datetime         not null
 #
 class User < ApplicationRecord
+  before_save {self.email = email.downcase}
   validates :first_name, presence: true, length: {maximum: 25}
   validates :last_name, presence: true, length: {maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
