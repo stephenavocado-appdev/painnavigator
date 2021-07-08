2dfc178afc2a3e456760ddd3e35780ff9424f612
stephenavocado <stephenwalter@chicagobooth.edu>
2021-07-08 15:55:50 +0000

    rearranged column name comments in new user model

--- app/models/new_user_survey.rb
+++ app/models/new_user_survey.rb
@@ -4,6 +4,14 @@
 #
 #  id         :integer          not null, primary key
 #  q_1        :string
+#  q_2        :string
+#  q_3        :string
+#  q_4        :string
+#  q_5        :string
+#  q_6        :string
+#  q_7        :string
+#  q_8        :string
+#  q_9        :string
 #  q_10       :string
 #  q_11       :string
 #  q_12       :string
@@ -14,18 +22,10 @@
 #  q_17       :string
 #  q_18       :string
 #  q_19       :string
-#  q_2        :string
 #  q_20       :string
 #  q_21       :string
 #  q_22       :string
 #  q_23       :string
-#  q_3        :string
-#  q_4        :string
-#  q_5        :string
-#  q_6        :string
-#  q_7        :string
-#  q_8        :string
-#  q_9        :string
 #  status     :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
