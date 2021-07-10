dcba08a72aa1ef03ec2787ac1a48d5c6f02a12a3
stephenavocado <stephenwalter@chicagobooth.edu>
2021-07-10 03:03:23 +0000

    removed unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
 
-  get("/", { :controller => "application", :action => "home"})
+  get '/'            => 'application#home'
   get 'user_sign_up' => 'user_authentication#sign_up_form'        
   post 'insert_user' => 'user_authentication#create'
-
-  # Routes for the New user survey resource:
-
-  # CREATE
-  # post("/insert_new_user_survey", { :controller => "new_user_surveys", :action => "create" })
-          
-  # READ
 
   get("/new_user/1", { :controller => "new_user_surveys", :action => "q_1" })
   get("/new_user/2", { :controller => "new_user_surveys", :action => "q_2" })
@@ -38,78 +31,6 @@ Rails.application.routes.draw do
   get("/new_user/23", { :controller => "new_user_surveys", :action => "q_23" })  
   get("/new_user/24", { :controller => "new_user_surveys", :action => "q_24" }) 
   get("/dashboard", { :controller => "application", :action => "dashboard" })
- 
-
-  
-  # UPDATE
-  
-  # post("/modify_new_user_survey/:path_id", { :controller => "new_user_surveys", :action => "update" })
-  
-  # DELETE
-  # get("/delete_new_user_survey/:path_id", { :controller => "new_user_surveys", :action => "destroy" })
-
-  #------------------------------
-  
-  
-  
-  # Routes for the Pain diary question resource:
-
-  # CREATE
-  post("/insert_pain_diary_question", { :controller => "pain_diary_questions", :action => "create" })
-          
-  # READ
-  get("/pain_diary_questions", { :controller => "pain_diary_questions", :action => "index" })
-  
-  get("/pain_diary_questions/:path_id", { :controller => "pain_diary_questions", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_pain_diary_question/:path_id", { :controller => "pain_diary_questions", :action => "update" })
-  
-  # DELETE
-  get("/delete_pain_diary_question/:path_id", { :controller => "pain_diary_questions", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Course resource:
-
-  # CREATE
-  post("/insert_course", { :controller => "courses", :action => "create" })
-          
-  # READ
-  get("/courses", { :controller => "courses", :action => "index" })
-  
-  get("/courses/:path_id", { :controller => "courses", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_course/:path_id", { :controller => "courses", :action => "update" })
-  
-  # DELETE
-  get("/delete_course/:path_id", { :controller => "courses", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Lesson resource:
-
-  # CREATE
-  post("/insert_lesson", { :controller => "lessons", :action => "create" })
-          
-  # READ
-  get("/lessons", { :controller => "lessons", :action => "index" })
-
-  #get("/lessons/getstarted"), { :controller => "lessons", :action => "first" })
-  
-  get("/lessons/:path_id", { :controller => "lessons", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_lesson/:path_id", { :controller => "lessons", :action => "update" })
-  
-  # DELETE
-  get("/delete_lesson/:path_id", { :controller => "lessons", :action => "destroy" })
-
-  #------------------------------
 
   # Routes for the Goal resource:
 
@@ -153,67 +74,8 @@ Rails.application.routes.draw do
 
   #------------------------------
 
-  # Routes for the Question resource:
-
-  # CREATE
-  post("/insert_question", { :controller => "questions", :action => "create" })
-          
-  # READ
-  get("/questions", { :controller => "questions", :action => "index" })
-  
-  get("/questions/:path_id", { :controller => "questions", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_question/:path_id", { :controller => "questions", :action => "update" })
-  
-  # DELETE
-  get("/delete_question/:path_id", { :controller => "questions", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Goal category resource:
-
-  # CREATE
-  post("/insert_goal_category", { :controller => "goal_categories", :action => "create" })
-          
-  # READ
-  #get("/goal_categories", { :controller => "goal_categories", :action => "index" })
-  
-  #get("/goal_categories/:path_id", { :controller => "goal_categories", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_goal_category/:path_id", { :controller => "goal_categories", :action => "update" })
-  
-  # DELETE
-  get("/delete_goal_category/:path_id", { :controller => "goal_categories", :action => "destroy" })
-
-  #------------------------------
-
-  # Routes for the Video resource:
-
-  # CREATE
-  post("/insert_video", { :controller => "videos", :action => "create" })
-          
-  # READ
-  get("/videos", { :controller => "videos", :action => "index" })
-  
-  get("/videos/:path_id", { :controller => "videos", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_video/:path_id", { :controller => "videos", :action => "update" })
-  
-  # DELETE
-  get("/delete_video/:path_id", { :controller => "videos", :action => "destroy" })
-
-  #------------------------------
-
   # Routes for the User account:
 
- 
-      
   # EDIT PROFILE FORM        
   get("/edit_user_profile", { :controller => "user_authentication", :action => "edit_profile_form" })       
   # UPDATE RECORD
