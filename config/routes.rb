09c16c658035bcda69b0bd6e4ed9e712ed407404
stephenavocado <stephenwalter@chicagobooth.edu>
2021-07-10 19:59:03 +0000

    fixed style tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,47 @@
 Rails.application.routes.draw do
 
-  get '/'             => 'application#home'
-  get 'dashboard'     => 'application#dashboard'
+  get '/'                     => 'application#home'
+  get 'dashboard'             => 'application#dashboard'
 
-  get 'user_sign_up'  => 'user_authentication#sign_up_form'        
-  post 'insert_user'  => 'user_authentication#create'
+  get 'user_sign_up'          => 'user_authentication#sign_up_form'        
+  post 'insert_user'          => 'user_authentication#create'
 
-  get 'new_user/1'    => 'new_user_surveys#q_1'
-  get 'new_user/2'    => 'new_user_surveys#q_2'
-  get 'new_user/3'    => 'new_user_surveys#q_3'
-  get 'new_user/4'    => 'new_user_surveys#q_4'
-  get 'new_user/5'    => 'new_user_surveys#q_5'
-  get 'new_user/6'    => 'new_user_surveys#q_6'
-  get 'new_user/7'    => 'new_user_surveys#q_7'
-  get 'new_user/8'    => 'new_user_surveys#q_8'
-  get 'new_user/9'    => 'new_user_surveys#q_9'
-  get 'new_user/10'   => 'new_user_surveys#q_10'
-  get 'new_user/11'   => 'new_user_surveys#q_11'
-  get 'new_user/12'   => 'new_user_surveys#q_12'
-  get 'new_user/13'   => 'new_user_surveys#q_13'
-  get 'new_user/14'   => 'new_user_surveys#q_14'
-  get 'new_user/15'   => 'new_user_surveys#q_15'
-  get 'new_user/16'   => 'new_user_surveys#q_16'
-  get 'new_user/17'   => 'new_user_surveys#q_17'
-  get 'new_user/18'   => 'new_user_surveys#q_18'
-  get 'new_user/19'   => 'new_user_surveys#q_19'
-  get 'new_user/20'   => 'new_user_surveys#q_20'
-  get 'new_user/21'   => 'new_user_surveys#q_21'
-  get 'new_user/22'   => 'new_user_surveys#q_22'
-  get 'new_user/23'   => 'new_user_surveys#q_23'
-  get 'new_user/24'   => 'new_user_surveys#q_24'
+  get 'new_user/1'            => 'new_user_surveys#q_1'
+  get 'new_user/2'            => 'new_user_surveys#q_2'
+  get 'new_user/3'            => 'new_user_surveys#q_3'
+  get 'new_user/4'            => 'new_user_surveys#q_4'
+  get 'new_user/5'            => 'new_user_surveys#q_5'
+  get 'new_user/6'            => 'new_user_surveys#q_6'
+  get 'new_user/7'            => 'new_user_surveys#q_7'
+  get 'new_user/8'            => 'new_user_surveys#q_8'
+  get 'new_user/9'            => 'new_user_surveys#q_9'
+  get 'new_user/10'           => 'new_user_surveys#q_10'
+  get 'new_user/11'           => 'new_user_surveys#q_11'
+  get 'new_user/12'           => 'new_user_surveys#q_12'
+  get 'new_user/13'           => 'new_user_surveys#q_13'
+  get 'new_user/14'           => 'new_user_surveys#q_14'
+  get 'new_user/15'           => 'new_user_surveys#q_15'
+  get 'new_user/16'           => 'new_user_surveys#q_16'
+  get 'new_user/17'           => 'new_user_surveys#q_17'
+  get 'new_user/18'           => 'new_user_surveys#q_18'
+  get 'new_user/19'           => 'new_user_surveys#q_19'
+  get 'new_user/20'           => 'new_user_surveys#q_20'
+  get 'new_user/21'           => 'new_user_surveys#q_21'
+  get 'new_user/22'           => 'new_user_surveys#q_22'
+  get 'new_user/23'           => 'new_user_surveys#q_23'
+  get 'new_user/24'           => 'new_user_surveys#q_24'
 
-  get 'creategoal'           => 'goals#selectcategory'
-  get 'creategoal/:category' => 'goals#creategoal'
-  post 'insert_goal'         => 'goals#create'
+  get 'creategoal'            => 'goals#selectcategory'
+  get 'creategoal/:category'  => 'goals#creategoal'
+  post 'insert_goal'          => 'goals#create'
           
   get 'goals'                 => 'goals#index'
   get 'goals/:path_id'        => 'goals#show'
   post 'modify_goal/:path_id' => 'goals#update'
   get 'delete_goal/:path_id'  => 'goals#destroy'
 
-  #------------------------------
-
-  # Routes for the Pain diary resource:
-
-  # CREATE
-  post("/insert_pain_diary", { :controller => "pain_diaries", :action => "create" })
-          
+  post 'insert_pain_diary'    => 'pain_diaries#create'
+            
   # READ
   get("/pain_diaries", { :controller => "pain_diaries", :action => "index" })
   
